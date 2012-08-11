7fdd5c7e53be3fcddd0b299736195a0316a91243
evanshultz <neaterbookmarks@gmail.com>
2012-08-11 00:01:22 -0700

    Fix crash due to old code. Update KR translation.

--- options.js
+++ options.js
@@ -159,9 +159,6 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 document.addEventListener('DOMContentLoaded', function(){
-	var resetButton = document.querySelectorAll('reset-button');
-	resetButton.addEventListener('click', trackButtonClick);
-	
 	// check if options can be saved locally
 	if (window.localStorage == null) {
 		alert("LocalStorage must be enabled for managing options.");
